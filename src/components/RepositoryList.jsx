d49c045f84eab15a3c94660d2a7c803cc61c9a97
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-11 15:53:37 -0300

    style list repository

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,10 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import { useState } from "react";
+
+// https://api.github.com/users/samucatezu/repos
+import "../styles/repositories.scss";
+
 const repository = {
   name: "unform",
   description: "Forms in React",
@@ -7,6 +12,8 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
   return (
     <section className="repository-lis t">
       <h1>Lista de repositorios</h1>
