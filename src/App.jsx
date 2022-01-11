e9fd0f1812c84ed4f2648e9a8f1de2015a7ecf7f
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-11 12:09:06 -0300

    component state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import { RepositoryList } from './components/RepositoryList';
-import './styles/global.scss';
+import { Counter } from "./components/counter";
+import { RepositoryList } from "./components/RepositoryList";
+import "./styles/global.scss";
 
-export function App(){  
-  return <RepositoryList />
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+      <Counter />
+    </>
+  );
 }
